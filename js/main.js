035ba72e61d959c7bd7edcaff2897bb950f0740e
Ousalem.david@netc.fr <you@example.com>
2017-07-18 12:07:45 +0200

    voici mon slider

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
-////////////////// Mes Variables/////////////////////////////////
-var tableaux =;
+// mes variables.
 
 
 
 
+var myImg = document.getElementById('monImage');
+var tab = ["cactus.jpg", "montagne.jpg", "bateau.jpg"];
+var count = 0;
+var suivant = document.getElementById('boutonsuivant');
+var pause = document.getElementById('boutonprecedent');
+
+// ma fonction
+
+ setInterval (function slide (){
+   myImg.src = "img/" + tab[count];
+   count++;
+   if(count> (tab.length -1)){
+
+     count = 0;
+   }
+ }, 2000);
+
+
+ function btn () {
+
+if ( suivant.onclick = slide());
+count++;
 
 
 
-////////////////////fin de mes variables///////////////////////////////
-///////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////
-//////////////////// Mes tableaux ////////////////////////////////////
-var image =  new Array ["montagne.jpg" , "cactus.jpg"];
 
+ }
 
+// mes boutons.
+// Si j'appuie sur le bouton '' suivant ''  le compteur doit ajouté une images avec
+// count++; il avance d'une image à chaque clique .
+// Si j'appuie sur le bouton '' précédent '' avec un count--;  il revient en arrière
+// d'une image a chaque clique .
 
-
-/////////////////// mes functions ////////////////////////////////////////
-
-function deplacer () {
-
-
-}
+//
